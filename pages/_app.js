4294efde1b285d863d8df8c4ecc79116133de4ed
Siarhei Davidouski <sergeyyellow@bk.ru>
2021-11-29 09:52:15 +0300

    Changes made

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-
 // Importing global styles
 import '../styles/globals.scss';
 
@@ -15,18 +13,7 @@ import '../components/one-product-card/one-product-card.scss';
 import '../components/one-filter-by-camera/one-filter-by-camera.scss';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-
-        {/* Connecting the Montserrat font */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
-    </>);
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -48,6 +48,13 @@ export default function Home({ data }) {
 			<Head>
 				<title>Filter for cameras</title>
 				<meta name="description" content="Filter for cameras" />
+
+				<link rel="icon" href="/favicon.ico" />
+				
+				{/* Connecting the Montserrat font */}
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+				<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;700&display=swap" rel="stylesheet" />
 			</Head>
 
 			<main className="grid-container">
